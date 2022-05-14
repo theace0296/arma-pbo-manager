a0a8cb486e8ff912468e3cf18f9473524d071200
Chase <29675446+theace0296@users.noreply.github.com>
2022-05-14 14:44:14 -0400

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# @theace0296/arma-pbo-manager
+A pure-JS implemenation of managing Arma pbo files.
---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+  create: (name, files, options = {}) => {
+
+  },
+  extract: (destination, options = {}) => {
+
+  },
+}
